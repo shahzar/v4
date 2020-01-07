1c3819666338d1c4645fb80bccb3f500fc950f2a
Shahzar Ahmed <shaz_ahd@outlook.com>
2020-01-07 21:31:25 +0400

    Updated manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'ShahzarAhmed',
+        short_name: 'ShahzarAhmed',
         start_url: '/',
         background_color: config.darkNavyColor,
         theme_color: config.navyColor,
